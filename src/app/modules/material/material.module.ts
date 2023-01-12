ff68f4c713d19f6e55f5668ccff0cf4103892b29
hernan castilla <hcastillaq@gmail.com>
2023-01-11 19:46:42 -0500

    delete angular flex-layout

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -14,7 +13,6 @@ const components: any = [
 	FormsModule,
 	ReactiveFormsModule,
 	RouterModule,
-	FlexLayoutModule,
 	MatButtonModule,
 	MatCardModule,
 	MatFormFieldModule,
